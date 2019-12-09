a2da51572911039c6d4346ad3bd7ba82718074cf
Mustafa İlhan <ilhan.mstf@gmail.com>
2019-12-09 08:15:49 +0100

    Question is added

--- day_1.js
+++ day_1.js
@@ -1,5 +1,6 @@
-// Input file: https://adventofcode.com/2019/day/1/input,
-// Run this code on browsers console.
+// Question: https://adventofcode.com/2019/day/1
+// Input file: https://adventofcode.com/2019/day/1/input
 
+// Run this code on browser's console
 let modules = document.querySelector("pre").innerText.split('\n').filter(m => m !== "")
 modules.map(m => Math.floor(m / 3) - 2).reduce((total, m) => total + m)
