1fa71a8db9bf5869fbc99badcfbefda2dee16d7e
Mustafa İlhan <ilhan.mstf@gmail.com>
2019-12-09 09:00:30 +0100

    Update as part 1

--- day_1.js
+++ day_1.js
@@ -2,5 +2,7 @@
 // Input file: https://adventofcode.com/2019/day/1/input
 
 // Run this code on browser's console
+
+// Part 1
 let modules = document.querySelector("pre").innerText.split('\n').filter(m => m !== "")
 modules.map(m => Math.floor(Number(m) / 3) - 2).reduce((total, m) => total + m)
